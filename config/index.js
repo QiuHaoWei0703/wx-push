6dc68fd53eb794db0179574947409e24c0d1d5e5
QiuHaoWei0703 <148554839+QiuHaoWei0703@users.noreply.github.com>
2023-10-20 21:23:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wxb6afb5923f16ebbd",
   appsecret: "81d7d0db8bd9f8998c6d9deac280a9ae",
   template_id: "P6D8g-nE_5nykuxOAiJFkDQl_GRryWT8Qh8YIyAMysA",
-  touser: "oRByR6fqql-HDu21WZQ3TAkZrujk",
+  touser: "oRByR6ecsCqPESJM6cDkkigrkPOg",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "青岛",
