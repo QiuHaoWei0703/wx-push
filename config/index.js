8983ed0a3b837823dbf398c1030f0a223201f74f
QiuHaoWei0703 <148554839+QiuHaoWei0703@users.noreply.github.com>
2023-10-20 20:55:32 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxb6afb5923f16ebbd",
+  appsecret: "81d7d0db8bd9f8998c6d9deac280a9ae",
+  template_id: "P6D8g-nE_5nykuxOAiJFkDQl_GRryWT8Qh8YIyAMysA",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "青岛",
+  first_date: "2023-10-20",
 };
